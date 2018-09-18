948e131eb6fe0cdac1e7c0cf5ef4d54e53134b5d
wave.songtao@gmail.com <wave.songtao@gmail.com>
2018-09-18 17:11:21 -0400

    fix indence

--- src/batch_processing/batch_processing.py
+++ src/batch_processing/batch_processing.py
@@ -14,50 +14,42 @@ class BatchProcessor:
 		"""
 		class constructor that initializes the instance according to the configurations of the S3 bucket, raw data and PostgreSQL table
 		:type s3_configfile:     str  path to S3 config file
-		:type schema_configfile: str  path to schema config file
 		:type psql_configfile:   str  path tp psql config file
 		"""
-        self.s3_config   = helper.parse_config(s3_configfile)
-        	#self.schema      = helpers.parse_config(schema_configfile)
-        self.psql_config = helper.parse_config(psql_configfile)
-        self.sc = SparkContext()
-        self.sc.setLogLevel("ERROR")
+		self.s3_config   = helper.parse_config(s3_configfile)
+		self.psql_config = helper.parse_config(psql_configfile)
+		self.sc = SparkContext()
+		self.sc.setLogLevel("ERROR")
 
 
-    def read_from_s3(self):
+	def read_from_s3(self):
 		"""
-    	reads files from s3 bucket defined by s3_configfile and creates Spark RDD
-    	"""
-    	yelp_business_filename = "s3a://{}/{}/{}".format(self.s3_config["BUCKET1"],
-    									   self.s3_config["FOLDER2"],
-    									   self.s3_config["RAW_DATA_FILE1"])
-    	yelp_rating_filename = "s3a://{}/{}/{}".format(self.s3_config["BUCKET2"],
-    									   self.s3_config["FOLDER2"],
-    									   self.s3_config["RAW_DATA_FILE2"])
-    	sanitory_inspection_filename = "s3a://{}/{}/{}".format(self.s3_config["BUCKET3"],
-    									   self.s3_config["FOLDER3"],
-    									   self.s3_config["RAW_DATA_FILE3"])
-    	self.df_yelp_business = self.sc.read.json(yelp_business_filename)
-    	self.df_yelp_rating = self.sc.read.json(yelp_rating_filename)
-    	self.df_sanitory_inspection = self.sc.read.csv(sanitory_inspection_filename)
+		reads files from s3 bucket defined by s3_configfile and creates Spark DataFrame
+		"""
+		yelp_business_filename = "s3a://{}/{}/{}".format(self.s3_config["BUCKET1"], self.s3_config["FOLDER2"], self.s3_config["RAW_DATA_FILE1"])
+		yelp_rating_filename = "s3a://{}/{}/{}".format(self.s3_config["BUCKET2"], self.s3_config["FOLDER2"], self.s3_config["RAW_DATA_FILE2"])
+		sanitory_inspection_filename = "s3a://{}/{}/{}".format(self.s3_config["BUCKET3"], self.s3_config["FOLDER3"], self.s3_config["RAW_DATA_FILE3"])
+		self.df_yelp_business = self.sc.read.json(yelp_business_filename)
+		self.df_yelp_rating = self.sc.read.json(yelp_rating_filename)
+		self.df_sanitory_inspection = self.sc.read.csv(sanitory_inspection_filename)
 
-
+    '''
 	def save_to_postgresql(self):
 		"""
 		save batch processing results into PostgreSQL database and adds necessary index
 		"""
 		config = {key: self.psql_config[key] for key in ["url", "driver", "user", "password"]}
 		config["dbtable"] = self.psql_config["dbtable_batch"]
-		postgres.save_to_postgresql(self.df, pyspark.sql.SQLContext(self.sc), configs, self.psql_config["mode_batch"])
-
+		postgre.save_to_postgresql(self.df, pyspark.sql.SQLContext(self.sc), configs, self.psql_config["mode_batch"])
+    '''
 
 	def run(self):
 		"""
-        executes the read from S3, transform by Spark and write to PostgreSQL database sequence
-        """
-        self.read_from_s3()
-        #self.spark_transform()
-        #self.save_to_postgresql()
+		executes the read from S3, transform by Spark and write to PostgreSQL database sequence
+		"""
+		self.read_from_s3()
+		#self.spark_transform()
+		#self.save_to_postgresql()
 		
 
 
