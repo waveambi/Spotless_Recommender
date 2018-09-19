e813dc474a8c44b03a07fd8a365377d16ffbac44
wave.songtao@gmail.com <wave.songtao@gmail.com>
2018-09-19 13:13:05 -0400

    change bash

--- src/spark-batch-run.sh
+++ src/spark-batch-run.sh
@@ -2,6 +2,7 @@
 S3CONFIGFILE=$PWD/config/s3bucket.config
 PSQLCONFIGFILE=$PWD/config/postgresql.config
 spark-submit --master spark://ip-10-0-0-7:7077 \
+             --jars $PWD/postgresql-42.2.2.jar \
              --driver-memory 4G \
              --executor-memory 4G \
              batch_processing/main_batch.py \
