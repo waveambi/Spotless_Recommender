ddf5ef3d329586a255d325794831f92b97e7e72b
wave.songtao@gmail.com <wave.songtao@gmail.com>
2018-09-27 13:05:31 -0400

    add streaming part

--- src/streaming/streamer.py
+++ src/streaming/streamer.py
@@ -128,12 +128,12 @@ class Streamer(SparkStreamerFromKafka):
         #rdd_bcast = (rdd.groupByKey().mapValues(lambda x: sorted(x, key=lambda el: el[0])).collect())
         # join the batch dataset with rdd_bcast, filter None values,
         # and from all the spot suggestions select specific for the driver to ensure no competition
-        self.resDF = self.dataStream.transform(lambda rdd: rdd.join(df_batch))#.reduceByKey(lambda x,y: x+y)
+        self.resDF = rdd.transform(lambda rdd: rdd.join(df_batch))#.reduceByKey(lambda x,y: x+y)
 
         # save data
         config = {key: self.psql_config[key] for key in
                   ["url", "driver", "user", "password", "mode_streaming", "dbtable_streaming", "nums_partition"]}
-        self.resDF.collect().toDF().write \
+        self.resDF.toDF().write \
             .format("jdbc") \
             .option("url", config["url"]) \
             .option("driver", config["driver"]) \
