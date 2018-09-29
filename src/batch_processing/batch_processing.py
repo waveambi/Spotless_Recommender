72d5d22ba65b8eeebac3ef96e684998eb8e211d6
wave.songtao@gmail.com <wave.songtao@gmail.com>
2018-09-29 16:15:47 -0400

    fix bugs

--- src/batch_processing/batch_processing.py
+++ src/batch_processing/batch_processing.py
@@ -10,7 +10,6 @@ from pyspark.sql import functions
 from pyspark.sql.window import Window
 from pyspark.sql.functions import rank, col
 
-
 from pyspark.ml import Pipeline
 from sparknlp.annotator import SentenceDetector, Tokenizer, Normalizer, Lemmatizer, SentimentDetector
 from sparknlp.base import DocumentAssembler, Finisher
@@ -50,18 +49,18 @@ class BatchProcessor:
         self.df_yelp_business = self.spark.read.json(yelp_business_filename)
         self.df_yelp_review = self.spark.read.json(yelp_rating_filename)
         self.df_sanitary = self.spark.read.csv(sanitary_inspection_filename, header=True)
-
+        self.trim_zipcode_udf = udf(lambda x: helper.trim_zipcode(x), StringType())
+        self.format_address_udf = udf(lambda x: helper.format_address(x), StringType())
+        self.format_name_udf = udf(lambda x: helper.format_name(x), StringType())
+        self.fuzzy_match_udf = udf(lambda x, y: helper.fuzzy_match(x, y), IntegerType())
+        self.convert_sentiment_udf = udf(lambda x: helper.convert_sentiment(x), IntegerType())
+        self.calculate_score_udf = udf(lambda x, y, z: helper.calculate_score(x, y, z), IntegerType())
 
     def spark_ranking_transform(self):
         """
         transforms Spark DataFrame with business dataset and sanitary inspection into cleaned data;
         adds information
         """
-        self.trim_zipcode_udf = udf(lambda x: helper.trim_zipcode(x), StringType())
-        self.format_address_udf = udf(lambda x: helper.format_address(x), StringType())
-        self.format_name_udf = udf(lambda x: helper.format_name(x), StringType())
-        self.fuzzy_match_udf = udf(lambda x, y: helper.fuzzy_match(x, y), IntegerType())
-
         self.df_sanitary = self.df_sanitary.select("Restaurant_Name", "Location_Name", "Category_Name", "Address", \
                                                    "City", "Zip", "Location_1", "Inspection_Demerits")
         self.df_sanitary = self.df_sanitary.withColumn("Zipcode", self.trim_zipcode_udf("Zip")).drop("Zip")
@@ -94,8 +93,7 @@ class BatchProcessor:
             .withColumnRenamed("avg(Avg_Inspection_Demerits)", "Avg_Inspection_Demerits") \
             .dropna()
         self.df_yelp_business_slice = self.df_yelp_business.select("business_id", "name", "address", "latitude",
-                                                                   "longitude",
-                                                                   "stars")
+                                                                   "longitude", "stars")
         self.df_ranking = self.df_ranking.join(self.df_yelp_business_slice,
                                                (self.df_ranking.business_id == self.df_yelp_business_slice.business_id),
                                                "inner").drop(self.df_ranking.business_id)
@@ -110,8 +108,6 @@ class BatchProcessor:
         self.sentiment_file = "s3a://{}/{}/{}".format(self.s3_config["BUCKET"], self.s3_config["TEXT_CORPUS_FOLDER"], \
                                                       self.s3_config["SENTIMENT_FILE"])
 
-
-        self.convert_sentiment_udf = udf(lambda x: helper.convert_sentiment(x), IntegerType())
         self.df_yelp_review = self.df_yelp_review \
             .select("user_id", "business_id", "stars", "text") \
             .withColumnRenamed("stars", "ratings")
@@ -136,7 +132,7 @@ class BatchProcessor:
         lemmatizer = Lemmatizer() \
             .setInputCols(["token"]) \
             .setOutputCol("lemma") \
-            .setDictionary(self.lemma_file, key_delimiter="", value_delimiter="\t")
+            .setDictionary(self.lemma_file, key_delimiter="->", value_delimiter="\t")
         sentiment_detector = SentimentDetector() \
             .setInputCols(["lemma", "sentence"]) \
             .setOutputCol("sentiment_score") \
@@ -158,13 +154,12 @@ class BatchProcessor:
         self.df_sentiment.cache()
 
         self.df_sentiment = self.df_sentiment.select(self.df_sentiment.business_id,
-                                           functions.when(self.df_sentiment.sentiment == "positive", 1).when(
-                                               self.df_sentiment.sentiment == "negative", -1).otherwise(0)) \
-                            .withColumnRenamed("CASE WHEN (sentiment = positive) THEN 1 WHEN (sentiment = negative) THEN -1 ELSE 0 END", "sentiment")
+                                                     functions.when(self.df_sentiment.sentiment == "positive", 1).when(
+                                                         self.df_sentiment.sentiment == "negative", -1).otherwise(0)) \
+            .withColumnRenamed("CASE WHEN (sentiment = positive) THEN 1 WHEN (sentiment = negative) THEN -1 ELSE 0 END",
+                               "sentiment")
         self.df_sentiment = self.df_sentiment.groupby("business_id").agg({"sentiment": "mean"}).withColumnRenamed(
             "avg(sentiment)", "avg_sentiment_score")
-
-
 
     def spark_create_block(self):
         self.determine_block_lat_ids_udf = udf(lambda z: helper.determine_block_lat_ids(z), IntegerType())
@@ -172,28 +167,26 @@ class BatchProcessor:
         self.df_ranking = self.df_ranking.withColumn("latitude_id", self.determine_block_lat_ids_udf("latitude"))
         self.df_ranking = self.df_ranking.withColumn("longitude_id", self.determine_block_log_ids_udf("longitude"))
 
-
     def spark_join_ranking_and_review(self):
         """
 
         :return:
         """
-        self.calculate_score_udf = udf(lambda x,y,z: helper.calculate_score(x,y,z), IntegerType())
         self.df = self.df_ranking \
             .join(self.df_sentiment, self.df_ranking.business_id == self.df_sentiment.business_id, 'inner') \
             .drop(self.df_sentiment.business_id) \
             .dropna()
-        avg_demerits = self.df.agg({"Avg_Inspection_Demerits": "mean"}).collect()[0][0] # around 6.5~
+        avg_demerits = self.df.agg({"Avg_Inspection_Demerits": "mean"}).collect()[0][0]  # around 6.5~
         print("Average score on sanitory inspections is ", avg_demerits)
         avg_rating = self.df.agg({"stars": "mean"}).collect()[0][0]
         print("Average rating on yelp review is ", avg_rating)
         avg_sentiment = self.df.agg({"avg_sentiment_score": "mean"}).collect()[0][0]
         print("Average sentiment on yelp review is ", avg_sentiment)
-        self.df = self.df.withColumn("score", self.calculate_score_udf("avg_sentiment_score", "stars", "Avg_Inspection_Demerits"))
+        self.df = self.df.withColumn("score", self.calculate_score_udf("avg_sentiment_score", "stars",
+                                                                       "Avg_Inspection_Demerits"))
         window = Window.partitionBy(self.df["latitude_id", "longitude_id"]).orderBy(self.df['score'].desc())
         self.df = self.df.select(["latitude_id", "longitude_id", "business_id", "name", "address", "latitude", \
-                    "longitude", "score"], rank().over(window).alias('rank')).filter(col('rank') <= 10)
-
+                                  "longitude", "score"], rank().over(window).alias('rank')).filter(col('rank') <= 10)
 
     def save_to_postgresql(self):
         """
@@ -211,7 +204,6 @@ class BatchProcessor:
             .mode(config["mode_batch"]) \
             .option("numPartitions", config["nums_partition"]) \
             .save()
-
 
     def run(self):
         """
