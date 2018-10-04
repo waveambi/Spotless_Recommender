d31b0e571aef8aced4e40de13a7993da5e16d911
wave.songtao@gmail.com <wave.songtao@gmail.com>
2018-10-04 15:21:02 -0400

    add id

--- src/streaming/streamer.py
+++ src/streaming/streamer.py
@@ -126,7 +126,7 @@ class Streamer(SparkStreamerFromKafka):
         self.resDF = rdd.join(self.df_ranking_result)
         if self.resDF.isEmpty():
             return
-        print(self.resDF.take(5))
+        print(self.resDF.take(5).toDF)
         config = {key: self.psql_config[key] for key in
                   ["url", "driver", "user", "password", "mode_batch", "dbtable_streaming", "nums_partition"]}
         self.resDF.toDF.write \
