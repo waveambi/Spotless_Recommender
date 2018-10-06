057f159dc747a04b276bac40729b625a70135754
wave.songtao@gmail.com <wave.songtao@gmail.com>
2018-10-06 00:49:25 -0400

    remove

--- src/batch_machine_learning/batch_collaborative_filtering.py
+++ src/batch_machine_learning/batch_collaborative_filtering.py
@@ -81,64 +81,40 @@ class BatchMachineLearning:
         self.df_yelp_rating = self.df_yelp_business \
                                   .join(self.df_yelp_review, self.df_yelp_business.business_id
                                         == self.df_yelp_review.business_id) \
-                                  .drop(self.df_yelp_review.business_id)
-
-        self.df_yelp_filter_user = self.df_yelp_rating \
-                                        .groupby("user_id") \
-                                        .agg({"business_id": "count"}) \
-                                        .withColumnRenamed("count(business_id)", "ratings_count")
-        self.df_yelp_filter_user = self.df_yelp_filter_user \
-                                        .filter(self.df_yelp_filter_user.ratings_count >= 1)
-        print("total number of users with more than 5 records is ", self.df_yelp_filter_user.count())
-
-        self.df_yelp_filter_business = self.df_yelp_rating \
-                                            .groupby("business_id") \
-                                            .agg({"user_id": "count"}) \
-                                            .withColumnRenamed("count(user_id)", "ratings_count")
-        self.df_yelp_filter_business = self.df_yelp_filter_business \
-                                            .filter(self.df_yelp_filter_business.ratings_count >= 1)
-        print("total number of restaurants with more than 1 users is ", self.df_yelp_filter_business.count())
-
-        self.df_yelp_rating_sample = self.df_yelp_rating \
-                                        .join(self.df_yelp_filter_user, self.df_yelp_rating.user_id
-                                              == self.df_yelp_filter_user.user_id, "inner") \
-                                        .drop(self.df_yelp_filter_user.user_id) \
-                                        .join(self.df_yelp_filter_business, self.df_yelp_rating.business_id
-                                              == self.df_yelp_filter_business.business_id, "inner") \
-                                        .drop(self.df_yelp_filter_business.business_id) \
-                                        .select("business_id", "user_id", "ratings")
+                                  .drop(self.df_yelp_review.business_id) \
+                                  .select("business_id", "user_id", "ratings")
 
         self.user_indexer = StringIndexer(inputCol="user_id", outputCol="user_id_indexed", handleInvalid='error')
         self.user_index = self.user_indexer \
-                                .fit(self.df_yelp_rating_sample.select("user_id")) \
-                                .transform(self.df_yelp_rating_sample.select("user_id"))
+                                .fit(self.df_yelp_rating.select("user_id")) \
+                                .transform(self.df_yelp_rating.select("user_id"))
 
-        self.df_yelp_rating_sample = self.df_yelp_rating_sample \
-                                        .join(self.user_index, self.df_yelp_rating_sample.user_id
+        self.df_yelp_rating = self.df_yelp_rating \
+                                        .join(self.user_index, self.df_yelp_rating.user_id
                                               == self.user_index.user_id, "inner") \
                                         .drop(self.user_index.user_id)
         self.business_indexer = StringIndexer(inputCol="business_id", outputCol="business_id_indexed",
                                               handleInvalid='error')
         self.business_index = self.business_indexer \
-                                    .fit(self.df_yelp_rating_sample.select("business_id")) \
-                                    .transform(self.df_yelp_rating_sample.select("business_id"))
-        self.df_yelp_rating_sample = self.df_yelp_rating_sample \
-                                        .join(self.business_index, self.df_yelp_rating_sample.business_id
+                                    .fit(self.df_yelp_rating.select("business_id")) \
+                                    .transform(self.df_yelp_rating.select("business_id"))
+        self.df_yelp_rating = self.df_yelp_rating \
+                                        .join(self.business_index, self.df_yelp_rating.business_id
                                               == self.business_index.business_id, "inner") \
                                         .drop(self.business_index.business_id)
 
-        self.df_yelp_rating_sample = self.df_yelp_rating_sample \
+        self.df_yelp_rating = self.df_yelp_rating \
                                         .withColumn("user_id_indexed",
-                                                self.df_yelp_rating_sample["user_id_indexed"].cast(IntegerType())) \
+                                                self.df_yelp_rating["user_id_indexed"].cast(IntegerType())) \
                                         .withColumn("business_id_indexed",
-                                                self.df_yelp_rating_sample["business_id_indexed"].cast(IntegerType()))
-        #self.df_yelp_rating_sample = self.df_yelp_rating_sample.rdd.repartition(48).toDF()
+                                                self.df_yelp_rating["business_id_indexed"].cast(IntegerType()))
+        #self.df_yelp_rating = self.df_yelp_rating.rdd.repartition(48).toDF()
 
     def spark_recommendation_prediction(self):
         """
         calculates restaurant recommendation and ranks with ALS Matrix Factorization
         """
-        self.df_training, self.df_test = self.df_yelp_rating_sample.randomSplit([0.8, 0.2])
+        self.df_training, self.df_test = self.df_yelp_rating.randomSplit([0.8, 0.2])
         self.df_training.cache()
         self.df_test.cache()
         als = ALS(maxIter=5, rank=10, coldStartStrategy="drop", userCol='user_id_indexed', itemCol='business_id_indexed',
